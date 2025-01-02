e492708c637261518b16dceb1aa57288079042d8
chamathishanka <pkchamath2000@gmail.com>
2025-01-02 23:31:19 +0530

    refactor layout and styles for App and SearchBar components

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ function App() {
       <div className="relative">
         <Navbar />
         <Hero />
-        <div className="absolute inset-x-0 top-[90%] h-screen sm:top-[60%] lg:top-[80%] mx-auto max-w-7xl px-4 sm:px-6 lg:px-0 py-6 pb-4">
+        <div className="relative z-20 inset-x-0 top-[80%] sm:top-[50%] lg:top-[50%] mx-auto max-w-7xl px-4 sm:px-6 lg:px-0 -mt-32">
           <TabsDemo />
         </div>
       </div>
       <div className="relative z-10 mt-8 mx-auto max-w-7xl px-4 sm:px-6 lg:px-0">
         <SearchBar />
       </div>
-      <div className="relative z-0 p-4 sm:p-8 lg:p-32">
+      <div className="relative z-0 p-4 sm:p-8 lg:p-32 -mt-16">
         <HowItWorks />
       </div>
       <Footer />
